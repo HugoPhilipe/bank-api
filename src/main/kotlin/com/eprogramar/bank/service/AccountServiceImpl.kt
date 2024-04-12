17f3e65ff2b0345e115d42b8f16619d63a580f04
HugoPhilipe <lipy_hugo@live.com>
2024-04-12 16:57:52 -0300

    unused imports

--- src/main/kotlin/com/eprogramar/bank/service/AccountServiceImpl.kt
+++ src/main/kotlin/com/eprogramar/bank/service/AccountServiceImpl.kt
@@ -5,7 +5,6 @@ import com.eprogramar.bank.repository.AccountRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PathVariable
 import java.util.*
 
 @Service
